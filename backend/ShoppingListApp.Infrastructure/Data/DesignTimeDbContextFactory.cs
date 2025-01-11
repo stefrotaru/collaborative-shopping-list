33d95fd738d0448497dc2467b82d3797d47d588c
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-01-11 13:33:56 +0200

    add DesignTimeDbContextFactory

---
+++ backend/ShoppingListApp.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ShoppingListContext>
+{
+    public ShoppingListContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ShoppingListContext>();
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CollaborativeShoppingListDb;Trusted_Connection=True;TrustServerCertificate=True");
+
+        return new ShoppingListContext(optionsBuilder.Options);
+    }
+}
