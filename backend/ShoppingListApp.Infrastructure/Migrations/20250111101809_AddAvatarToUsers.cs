13641388d058b0ac06793b24e9fc18b76ad95979
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-01-11 13:36:06 +0200

    generate AddAvatarToUsers migration files

---
+++ backend/ShoppingListApp.Infrastructure/Migrations/20250111101809_AddAvatarToUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingListApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAvatarToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Users");
+        }
+    }
+}
