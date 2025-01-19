edbda3df5179edb8c366bda911758984797268d5
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-01-19 22:52:54 +0200

    add shoppingLists store actions

--- client/src/store/shoppingLists.js
+++ client/src/store/shoppingLists.js
@@ -21,7 +21,7 @@ export const useShoppingListsStore = defineStore("shoppingListsStore", () => {
       }
 
       userShoppingLists.value = await response.json();
-      
+
       return userShoppingLists.value;
     } catch (error) {}
   };
@@ -39,7 +39,7 @@ export const useShoppingListsStore = defineStore("shoppingListsStore", () => {
           body: JSON.stringify({
             name: listName,
             groupId: groupId,
-            createdById: userId
+            createdById: userId,
           }),
         }
       );
@@ -50,12 +50,83 @@ export const useShoppingListsStore = defineStore("shoppingListsStore", () => {
 
       return await response.json();
     } catch (error) {}
+  };
+
+  const deleteList = async (listId) => {
+    try {
+      const response = await fetch(
+        `CollaborativeShoppingListAPI/ShoppingLists/${listId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id: listId }),
+        }
+      );
+    } catch (error) {
+      console.error("Error deleting list:", error);
+    }
+  }
+
+  const createGroup = async (groupName, description, createdById) => {
+    try {
+      const response = await fetch(`CollaborativeShoppingListAPI/Groups`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: groupName,
+          description,
+          createdById
+        }),
+      });
+
+      console.log(groupName, description, createdById);
+
+      if (!response.ok) {
+        throw new Error("Failed to create group");
+      }
+    } catch (error) {
+      console.error("Error creating group:", error);
+    }
+  };
+
+  const getListItems = async (listId) => {
+    try {
+      const response = await fetch(
+        `/CollaborativeShoppingListAPI/ShoppingItems/shoppinglist/${listId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch shopping list items");
+      }
+
+      return await response.json();
+    } catch (error) {}
+  };
+
+  const populateStore = async () => {
+    await fetchGroupShoppingLists(4); // TODO: Should fetch all groupId's for logged user
   }
 
   return {
     userShoppingLists,
     fetchGroupShoppingLists,
 
-    createNewList
+    createGroup,
+    createNewList,
+    deleteList,
+
+    getListItems,
+
+    populateStore
   };
 });
