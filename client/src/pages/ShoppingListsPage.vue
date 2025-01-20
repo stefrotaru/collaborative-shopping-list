ceb20a78abdae4dc2e0e9037dcdd29aae8e2a119
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-01-20 21:46:24 +0200

    improve user groups UI; fetch data; add createGroup

--- client/src/pages/ShoppingListsPage.vue
+++ client/src/pages/ShoppingListsPage.vue
@@ -1,44 +1,38 @@
 <template>
   <div class="shopping-lists-page">
     <h1>My Shopping Lists</h1>
-    <!-- <DataTable :value="shoppingLists" responsiveLayout="scroll">
-      <Column field="name" header="Name"></Column>
-      <Column field="createdAt" header="Created At">
-        <template #body="slotProps">
-          {{ formatDate(slotProps.data.createdAt) }}
-        </template>
-      </Column>
-      <Column header="Actions">
-        <template #body="slotProps">
-          <Button
-            icon="pi pi-eye"
-            class="p-button-rounded p-button-info mr-2"
-            @click="viewList(slotProps.data)"
-          />
-          <Button
-            icon="pi pi-trash"
-            class="p-button-rounded p-button-danger"
-            @click="confirmDeleteList(slotProps.data)"
-          />
-        </template>
-      </Column>
-    </DataTable> -->
 
     <div class="user-groups__container">
-      <div class="user-groups-header">
-        <h3>My Groups</h3>
-      </div>
       <div class="user-groups__list">
-        <div
-          class="user-groups__list-item"
-          v-for="group in userGroups"
-          :key="group.id"
-        >
-          <div class="user-groups__list-item-name">{{ group.name }}</div>
-          <div class="user-groups__list-item-actions">
-            <router-link :to="'/groups/' + group.id">Manage</router-link>
-          </div>
+        <div class="user-groups__list-header">
+          <h3>My Groups</h3>
+          <Button
+            label="Create Group"
+            icon="pi pi-plus"
+            @click="openCreateGroupDialog"
+          />
         </div>
+        <div class="user-groups__list__create-group">
+          <InputText
+            v-if="showCreateGroupDialog"
+            id="group-name"
+            v-model="newGroupName"
+            @keyup.enter="createGroup"
+          />
+        </div>
+        <ul>
+          <li
+            v-for="group in userGroups"
+            :key="group.id"
+          >
+            <div class="user-groups__list__item">
+              <div class="user-groups__list__item-name">{{ group.name }}</div>
+              <div class="user-groups__list__item-actions">
+                <router-link :to="'/groups/' + group.id">Manage</router-link>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
 
@@ -84,11 +78,13 @@
 
 <script setup>
 import { ref, onMounted, watch } from "vue";
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 import { useAuthStore } from "../store/auth";
 import Button from "primevue/button";
 import CreateListDialog from "../components/CreateListDialog.vue";
 import { useShoppingListsStore } from "../store/shoppingLists";
+
+import InputText from "primevue/inputtext";
 
 const router = useRouter();
 const authStore = useAuthStore();
@@ -97,6 +93,23 @@ const shoppingListsStore = useShoppingListsStore();
 const userGroups = ref([]);
 const shoppingLists = ref([]);
 const showCreateListDialog = ref(false);
+
+const showCreateGroupDialog = ref(false);
+const newGroupName = ref("");
+
+const openCreateGroupDialog = () => {
+  showCreateGroupDialog.value = true;
+};
+const createGroup = () => {
+  shoppingListsStore.createGroup(newGroupName.value, 'description', authStore.authenticatedUser.id);
+  newGroupName.value = "";
+  showCreateGroupDialog.value = false;
+
+  // fetch data again to get the updated list of groups
+  setTimeout(() => {
+    fetchData();
+  }, 1000);
+};
 
 const openCreateListDialog = () => {
   showCreateListDialog.value = true;
@@ -121,8 +134,13 @@ const onListCreated = async (listName) => {
     const createdList = await response.json();
     shoppingLists.value.push(createdList);
   } catch (error) {
-    console.error("Error creating shopping list:", error);
+    // console.error("Error creating shopping list:", error);
   }
+
+  // fetch data again to get the updated list of groups
+  setTimeout(() => {
+    fetchData();
+  }, 1000);
 
   //TODO: navigate to the created list
 };
@@ -133,24 +151,34 @@ const formatDate = (date) => {
 
 const viewList = (list) => {
   // Navigate to the shopping list page for the selected list
-  router.push(`/shopping-lists/${list.id}`);
+  router.push(`/shoppinglists/${list.id}`);
 };
 
 const confirmDeleteList = (list) => {
   // TODO: Implement delete list confirmation and functionality
   console.log("Delete list:", list);
+  shoppingListsStore.deleteList(list.id);
+
+  // fetch data again to get the updated list of groups
+  setTimeout(() => {
+    fetchData();
+  }, 1000);
 };
 
 const fetchData = async () => {
+  //TODO: first I should get all the groups and then fetch the shopping lists for all groups
   if (authStore.authenticatedUser) {
     try {
       const [groupsData, listsData] = await Promise.all([
         authStore.getUserGroups(),
-        shoppingListsStore.fetchGroupShoppingLists(3)
+        shoppingListsStore.fetchGroupShoppingLists(authStore.userGroups[0].id), //TODO: fetch shopping lists for all groups
       ]);
-      
+
       userGroups.value = groupsData;
       shoppingLists.value = listsData;
+
+      console.log("userGroups", userGroups.value);
+      console.log("shoppingLists", shoppingLists.value);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -176,53 +204,8 @@ watch(
   text-align: center;
 }
 
+.shopping-lists,
 .user-groups {
-  &__container {
-    max-width: 800px;
-    margin: 0 auto;
-  }
-
-  &-header {
-    font-size: 1.5rem;
-    font-weight: bold;
-    margin-bottom: 1rem;
-  }
-
-  &__list {
-    margin-top: 1rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    padding: 1rem;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-
-    display: grid;
-    grid-gap: 10px;
-    grid-template-columns: repeat(auto-fill, 32%);
-
-    &__item {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding: 0.5rem 1rem;
-      border-bottom: 1px solid #ccc;
-
-      &:last-child {
-        border-bottom: none;
-      }
-
-      &-name {
-        font-size: 1.2rem;
-      }
-
-      &-actions {
-        display: flex;
-        gap: 0.5rem;
-      }
-    }
-  }
-}
-
-.shopping-lists {
   &__container {
     max-width: 800px;
     margin: 0 auto;
