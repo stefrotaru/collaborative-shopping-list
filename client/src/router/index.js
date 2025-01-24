2b9ad80556883a89a0ceb910967c54fb68245c76
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-01-24 13:13:04 +0200

    add redirects;

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,41 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/HomePage.vue';
-import LoginPage from '../pages/LoginPage.vue';
-import RegisterPage from '../pages/RegistrationPage.vue';
-import UserProfilePage from '../pages/UserProfilePage.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import LoginPage from "../pages/LoginPage.vue";
+import RegisterPage from "../pages/RegistrationPage.vue";
+import UserProfilePage from "../pages/UserProfilePage.vue";
 
-import ShoppingLists from '../pages/ShoppingListsPage.vue';
-import ShoppingListPage from '../pages/ShoppingListPage.vue';
+import ShoppingLists from "../pages/ShoppingListsPage.vue";
+import ShoppingListPage from "../pages/ShoppingListPage.vue";
+
+import { useAuthStore } from "../store/auth.js";
+// import { useGlobalStore } from "../store/global.js";
+import { useShoppingListsStore } from "../store/shoppingLists.js";
 
 const routes = [
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
+    path: "/home",
+    name: "Home",
+    component: HomePage,
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: LoginPage
+    path: "/login",
+    name: "Login",
+    component: LoginPage,
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: RegisterPage
+    path: "/register",
+    name: "Register",
+    component: RegisterPage,
   },
   {
-    path: '/profile',
-    name: 'Profile',
+    path: "/profile",
+    name: "Profile",
     component: UserProfilePage,
   },
   {
-    path: '/shoppinglists',
-    name: 'ShoppingLists',
+    path: "/shoppinglists",
+    name: "ShoppingLists",
     component: ShoppingLists,
   },
   {
-    path: '/shoppinglists/:id',
-    name: 'ShoppingList',
+    path: "/shoppinglists/:id",
+    name: "ShoppingList",
     component: ShoppingListPage,
   },
 ];
@@ -43,6 +47,28 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore();
+  const publicRoutes = ['/login', '/register'];
+  
+  if (!authStore.authenticatedUser && !publicRoutes.includes(to.path)) {
+    console.log('🔎🔎🔎check auth needed!')
+    await authStore.checkAuth();
+  }
+
+  console.log("change route");
+  console.log(authStore.authenticatedUser);
+
+  if (
+    to.path === "/login" ||
+    (to.path === "/register" && authStore.authenticatedUser)
+  ) {
+    next("/home");
+  } else {
+    next();
+  }
 });
 
 export default router;
