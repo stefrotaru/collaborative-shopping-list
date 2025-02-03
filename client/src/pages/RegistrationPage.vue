b909e7e811b6b4d6f2716db5fbf09cedee8b991b
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-02-03 21:30:16 +0200

    add avatar selection ui

--- client/src/pages/RegistrationPage.vue
+++ client/src/pages/RegistrationPage.vue
@@ -8,6 +8,25 @@
     <div class="card">
       <form @submit.prevent="handleRegistration">
         <div class="card__form-group">
+          <Carousel
+            :value="avatarItems"
+            :numVisible="1"
+            :numScroll="1"
+            :circular="true"
+            :showIndicators="false"
+            v-model:page="currentAvatarPage"
+          >
+            <template #item="slotProps">
+              <div class="avatar">
+                <img
+                  :src="`/avatars/${slotProps.data.file}`"
+                  :alt="`Avatar ${slotProps.data.id}`"
+                />
+              </div>
+            </template>
+          </Carousel>
+        </div>
+        <div class="card__form-group">
           <label for="username">Username</label>
           <InputText id="username" v-model="username" required />
         </div>
@@ -30,34 +49,51 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
-import { useAuthStore } from '../store/auth';
+import { useAuthStore } from "../store/auth";
+
 import InputText from "primevue/inputtext";
 import Password from "primevue/password";
 import Button from "primevue/button";
+import Carousel from "primevue/carousel";
+
+import avatarEnum from "../data/avatars";
+const avatarItems = avatarEnum.map((avatar) => ({
+  ...avatar,
+  itemImageSrc: `/avatars/${avatar.file}`,
+}));
+const currentAvatarPage = ref(0)
+const selectedAvatar = computed(() => avatarItems[currentAvatarPage.value].file);
+
+
+console.log("avatarEnum", avatarEnum);
 
 const authStore = useAuthStore();
 const username = ref("");
 const email = ref("");
 const password = ref("");
 const confirmPassword = ref("");
-const avatar = 'default_avatar.png'; //TODO: Add avatar selection
 const router = useRouter();
 
 const handleRegistration = async () => {
-  const registerResponse = await authStore.register(username.value, email.value, password.value, avatar);
-  
+  const registerResponse = await authStore.register({
+    username: username.value,
+    email: email.value,
+    password: password.value,
+    avatar: selectedAvatar.value
+  });
+
   if (!registerResponse) {
     return;
   }
 
-  username.value = '';
-  email.value = '';
-  password.value = '';
-  confirmPassword.value = '';
+  username.value = "";
+  email.value = "";
+  password.value = "";
+  confirmPassword.value = "";
 
   // TODO: Navigate to appropriate page after successful login
-  router.push('/home');
+  router.push("/home");
 };
 </script>
