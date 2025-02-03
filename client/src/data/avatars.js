3c230848abf40b74d9f8dcbff21cb1e085ab92c0
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-02-03 21:35:21 +0200

    add avatars

---
+++ client/src/data/avatars.js
@@ -0,0 +1,17 @@
+const Avatars = [
+    { id: 0, file: 'default_avatar.png' },
+    { id: 1, file: 'anime-away-face-svgrepo-com.svg' },
+    { id: 2, file: 'avatar-avocado-food-svgrepo-com.svg' },
+    { id: 3, file: 'avatar-bad-breaking-svgrepo-com.svg' },
+    { id: 4, file: 'avatar-batman-comics-svgrepo-com.svg' },
+    { id: 5, file: 'avatar-cacti-cactus-svgrepo-com.svg' },
+    { id: 6, file: 'avatar-cloud-crying-svgrepo-com.svg' },
+    { id: 7, file: 'avatar-coffee-cup-svgrepo-com.svg' },
+    { id: 8, file: 'avatar-dead-monster-svgrepo-com.svg' },
+    { id: 9, file: 'avatar-joker-squad-svgrepo-com.svg' },
+    { id: 10, file: 'avatar-lazybones-sloth-svgrepo-com.svg' },
+    { id: 11, file: 'avatar-male-ozzy-svgrepo-com.svg' },
+    { id: 12, file: 'friday-halloween-jason-svgrepo-com.svg' },
+];
+
+export default Avatars;
