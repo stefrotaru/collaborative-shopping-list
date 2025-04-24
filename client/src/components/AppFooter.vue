361b4cc318d5ea7de045e08c9c2cdb64dc41c9bf
Stefan Rotaru <stefanrotaru@hotmail.com>
2025-04-24 19:11:47 +0300

    comment out footer links section for future use

--- client/src/components/AppFooter.vue
+++ client/src/components/AppFooter.vue
@@ -2,12 +2,12 @@
   <footer class="app-footer">
     <div class="footer-content">
       <div class="footer-logo">Link-List</div>
-      <div class="footer-links">
+      <!-- <div class="footer-links">
         <a href="#" @click.prevent="navigateTo('/about')">About</a>
         <a href="#" @click.prevent="navigateTo('/privacy')">Privacy</a>
         <a href="#" @click.prevent="navigateTo('/terms')">Terms</a>
         <a href="#" @click.prevent="navigateTo('/contact')">Contact</a>
-      </div>
+      </div> -->
       <div class="footer-copyright">
         &copy; {{ new Date().getFullYear() }} Link-List App by Stefan Rotaru. All rights reserved.
       </div>
